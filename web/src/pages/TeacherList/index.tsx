72339317f03e11360ef63d01d557781fe0bfd864
laercio candido <lcs_junior@yahoo.com.br>
2020-08-06 03:42:58 -0300

    Página de cadastro do professor

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 import './styles.css';
 
 function TeacherList() {
@@ -8,18 +10,37 @@ function TeacherList() {
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Estes são os proffys disponíveis">
 				<form id="search-teachers">
-					<div className="input-block">
-						<label htmlFor="subject">Matéria</label>
-						<input type="text" id="subject" />
-					</div>
-					<div className="input-block">
-						<label htmlFor="week-day">Dia da semana</label>
-						<input type="text" id="week-day" />
-					</div>
-					<div className="input-block">
-						<label htmlFor="time">Horário</label>
-						<input type="text" id="time" />
-					</div>
+					<Select
+						name="subject"
+						label="Matéria"
+						options={[
+							{ value: 'Arte', label: 'Arte' },
+							{ value: 'Biologia', label: 'Biologia' },
+							{ value: 'Ciências', label: 'Ciências' },
+							{ value: 'Educação Física', label: 'Educação Física' },
+							{ value: 'Física', label: 'Física' },
+							{ value: 'Geografia', label: 'Geografia' },
+							{ value: 'História', label: 'História' },
+							{ value: 'Inglês', label: 'Inglês' },
+							{ value: 'Matemática', label: 'Matemática' },
+							{ value: 'Português', label: 'Português' },
+							{ value: 'Química', label: 'Química' },
+						]}
+					/>
+					<Select
+						name="week-day"
+						label="Dia da semana"
+						options={[
+							{ value: '0', label: 'Domingo' },
+							{ value: '1', label: 'Segunda-feira' },
+							{ value: '2', label: 'Terça-feira' },
+							{ value: '3', label: 'Quarta-feira' },
+							{ value: '4', label: 'Quinta-feira' },
+							{ value: '5', label: 'Sexta-feira' },
+							{ value: '6', label: 'Sábado' },
+						]}
+					/>
+					<Input type="time" name="time" label="Horário"/>
 				</form>
 			</PageHeader>
 
